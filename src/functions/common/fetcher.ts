55507cd8bad86903ddb664278252c25626463319
naufalzhafran <latifnaufal7@gmail.com>
2025-01-28 13:37:11 +0700

    fix: resolve bad fecther function error handler

--- src/functions/common/fetcher.ts
+++ src/functions/common/fetcher.ts
@@ -1,18 +1,22 @@
 export default async function fetcher<T>(input: string, init?: RequestInit) {
   const response = await fetch(input, init);
-
-  const parsedResponse = (await response.json()) as T;
-
-  if (response.ok) {
-    return parsedResponse;
-  } else {
-    const error =
-      (parsedResponse &&
-        typeof parsedResponse === "object" &&
-        "message" in parsedResponse &&
-        parsedResponse?.message) ||
-      response.statusText;
-      
-    return Promise.reject(error);
+  
+  try {
+    const parsedResponse = await response.json() as T;
+    
+    if (response.ok) {
+      return parsedResponse;
+    } else {
+      const error =
+        (parsedResponse &&
+          typeof parsedResponse === "object" &&
+          "message" in parsedResponse &&
+          parsedResponse?.message) ||
+        response.statusText;
+        
+      return Promise.reject(error);
+    }
+  } catch (error) {
+    return Promise.reject(response.statusText || 'Failed to parse response');
   }
 }
