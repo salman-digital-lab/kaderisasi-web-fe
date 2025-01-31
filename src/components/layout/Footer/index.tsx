98320830afa8db963220b958ba7dfa5bf6da23ed
naufalzhafran <latifnaufal7@gmail.com>
2025-01-31 13:15:20 +0700

    refactor: Update Footer component with minor content and styling changes

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -10,26 +10,9 @@ import {
 import logo from "@/assets/bmka_logo_color.png";
 
 import classes from "./index.module.css";
+import Link from "next/link";
 
 const data = [
-  {
-    title: "About",
-    links: [
-      { label: "Features", link: "#" },
-      { label: "Pricing", link: "#" },
-      { label: "Support", link: "#" },
-      { label: "Forums", link: "#" },
-    ],
-  },
-  {
-    title: "Project",
-    links: [
-      { label: "Contribute", link: "#" },
-      { label: "Media assets", link: "#" },
-      { label: "Changelog", link: "#" },
-      { label: "Releases", link: "#" },
-    ],
-  },
   {
     title: "Community",
     links: [
@@ -42,27 +25,6 @@ const data = [
 ];
 
 export default function Footer() {
-  const groups = data.map((group) => {
-    const links = group.links.map((link, index) => (
-      <Text<"a">
-        key={index}
-        className={classes.link}
-        component="a"
-        href={link.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </Text>
-    ));
-
-    return (
-      <div className={classes.wrapper} key={group.title}>
-        <Text className={classes.title}>{group.title}</Text>
-        {links}
-      </div>
-    );
-  });
-
   return (
     <footer className={classes.footer}>
       <Container className={classes.inner}>
@@ -81,13 +43,13 @@ export default function Footer() {
               Jl. Ganesa No.7, Lb. Siliwangi, Kecamatan Coblong, Kota Bandung,
               Jawa Barat 40132
             </Text>
-            <Text className={classes.link}>+62 821 1128 6037</Text>
+            <Text className={classes.link}>+62 851-5616-8499</Text>
           </div>
         </div>
       </Container>
       <Container className={classes.afterFooter}>
         <Text c="dimmed" size="sm">
-          © 2024 BMKA Salman ITB All rights reserved.
+          © 2025 BMKA Salman ITB All rights reserved.
         </Text>
 
         <Group
@@ -96,19 +58,37 @@ export default function Footer() {
           justify="flex-end"
           wrap="nowrap"
         >
-          <ActionIcon size="lg" color="gray" variant="subtle">
+          <ActionIcon
+            component={Link}
+            href="#"
+            size="lg"
+            color="gray"
+            variant="subtle"
+          >
             <IconBrandTwitter
               style={{ width: rem(18), height: rem(18) }}
               stroke={1.5}
             />
           </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
+          <ActionIcon
+            component={Link}
+            href="#"
+            size="lg"
+            color="gray"
+            variant="subtle"
+          >
             <IconBrandYoutube
               style={{ width: rem(18), height: rem(18) }}
               stroke={1.5}
             />
           </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
+          <ActionIcon
+            component={Link}
+            href="#"
+            size="lg"
+            color="gray"
+            variant="subtle"
+          >
             <IconBrandInstagram
               style={{ width: rem(18), height: rem(18) }}
               stroke={1.5}
