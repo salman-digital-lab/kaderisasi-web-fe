ed481bfac52b19defd3739fe5ffb67297ed85415
naufalzhafran <latifnaufal7@gmail.com>
2025-07-10 21:17:57 +0700

    feat: Add empty state message for Ruang Curhat list when no data is available

--- src/features/profile/RuangCurhatList/index.tsx
+++ src/features/profile/RuangCurhatList/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Flex, Paper } from "@mantine/core";
+import { Flex, Paper, Stack, Text } from "@mantine/core";
 import { RuangCurhatData } from "@/types/model/ruangcurhat";
 import RuangCurhatCard from "@/components/common/RuangCurhatCard";
 
@@ -9,6 +9,18 @@ type PersonalActivityDataProps = {
 };
 
 export default function RuangCurhatList({ data }: PersonalActivityDataProps) {
+  if (data.length === 0) {
+    return (
+      <Paper radius="md" withBorder p="lg">
+        <Stack align="center" justify="center" h={200}>
+          <Text size="lg" c="dimmed">
+            Belum pernah mengikuti ruang curhat
+          </Text>
+        </Stack>
+      </Paper>
+    );
+  }
+  
   return (
     <Paper radius="md" withBorder p="lg">
       <Flex direction="column" gap="md">
